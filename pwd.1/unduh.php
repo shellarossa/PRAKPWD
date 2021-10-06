c125544472c09bbf92d43344f6ab80ab04fb0c4e
Shella Rossa Sukmawati <48092469+shellarossa@users.noreply.github.com>
2021-10-06 22:05:15 +0700

    Add files via upload

---
+++ pwd.1/unduh.php
@@ -0,0 +1,9 @@
+<?
+$myDir = "c:/wamp/www/NIM/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
